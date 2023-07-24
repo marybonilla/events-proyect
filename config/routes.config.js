ef27511f8106e4f64952000276d8b4200d24a112
Mary Bonilla <marybonillaseverino@gmail.com>
2023-07-25 01:04:16 +0200

    auth controller

---
+++ config/routes.config.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const miscController = require ('../controllers/misc.controller');
+const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/users.controller');
+
+router.get('/' , miscController.getHome);
+
+// Auth
+
+router.get('/register', authController.register);
+router.post('/register', authController.doRegister);

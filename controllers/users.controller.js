4246c2023c2efe87dc2f34a68de1830fb049c3ff
Mary Bonilla <marybonillaseverino@gmail.com>
2023-08-25 17:11:24 +0200

    poniendo la fecha de creacion del usuario en la vista

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,14 +5,17 @@ module.exports.profile = (req, res, next) => {
   res.render('user/profile', { user: req.user });
 }
 
-module.exports.getUserProfile = (req, res, next) => {
-    User.findById(req.params.id)
-      .then(user => {
-        if (user) {
-          res.render('user/profile', { user });
-        } else {
-          next(createError(404, 'User not found'))
-        }
-      })
-      .catch(next)
+module.exports.getUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+      const formattedCreatedAt = user.createdAt.toLocaleDateString();
+      res.render('user/profile', { user, formattedCreatedAt });
+    } else {
+      next(createError(404, 'User not found'))
+    }
+  } catch (error) {
+    next(error);
   }
+}
